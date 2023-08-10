81dabf7248a4340edfb58611d1909c9b8616a1b8
Gregory Echelberry <gechelberry@aol.clom>
2023-08-10 09:31:40 -0400

    Made readerpane a tabbed pane

--- src/main/java/Views/Application.java
+++ src/main/java/Views/Application.java
@@ -1,4 +1,6 @@
 package main.java.Views;
+import main.java.Data.Bible;
+import main.java.Service.DatabaseConnection;
 import main.java.Service.ProcessJSON;
 
 import javax.swing.*;
@@ -8,28 +10,33 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.util.Vector;
 
 public class Application extends JFrame {
 
     File selectedFile;
-    ReaderPanel readerPanel;
+    Vector<Bible> bibles;
     JSplitPane splitPane;
+    JTabbedPane tabbedPane;
+    JPanel emptyPanel;
+
     public Application() {}
     public boolean setUpFrame() {
         BorderLayout layout = new BorderLayout();
         layout.maximumLayoutSize(this);
         this.setTitle("Bible Application");
         this.setMinimumSize(new Dimension(1024, 768));
-        this.setSize(800,600);
+        this.setSize(1024,768);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLayout(layout);
+        getBibles();
         menuBar();
         toolbar();
         NavigationPane navigationPane = new NavigationPane(this);
         navigationPane.setVisible(true);
-        readerPanel = new ReaderPanel((long)1, (long)1, (long)1);
-        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, navigationPane, readerPanel);
+        createReaderTabbedPane();
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, navigationPane, tabbedPane);
         this.getContentPane().add(splitPane);
         this.pack();
         this.addWindowListener(new WindowAdapter() {
@@ -43,7 +50,7 @@ public class Application extends JFrame {
         Desktop desktop = Desktop.getDesktop();
         if (desktop.isSupported(Desktop.Action.APP_QUIT_HANDLER)) {
             desktop.setQuitHandler((e, response) -> {
-                boolean canQuit = true; // Enables or Disables Menu Quit command on Mac OS X
+                boolean canQuit = System.getProperty("os.name").equals("Mac OS X"); // Enables or Disables Menu Quit command on Mac OS X
                 if (canQuit) {
                     response.performQuit();
                 } else
@@ -58,9 +65,7 @@ public class Application extends JFrame {
         // File Menu
         JMenu file = new JMenu("File");
         JMenuItem importBible = new JMenuItem("Import Bible...");
-        importBible.addActionListener(e -> {
-            importDialog();
-        });
+        importBible.addActionListener(e -> importDialog());
         file.add(importBible);
 
 
@@ -79,6 +84,8 @@ public class Application extends JFrame {
 
         // View Menu
         JMenu view = new JMenu("View");
+        JMenuItem addBible = new JMenuItem("Open new Bible");
+        addBible.addActionListener(event -> addBibleDialog());
         JMenuItem navView = new JMenuItem("Show/Hide Nav Pane");
         navView.addActionListener(e -> {
             if (splitPane.getLeftComponent().isVisible()) {
@@ -88,6 +95,9 @@ public class Application extends JFrame {
                 splitPane.setDividerLocation(200);
             }
         });
+
+        view.add(addBible);
+        view.addSeparator();
         view.add(navView);
 
         menu.add(view);
@@ -115,19 +125,75 @@ public class Application extends JFrame {
         showNavPane.setToolTipText("Show/Hide Navigation Pane");
         toolBar.add(showNavPane);
 
+        // Add Separator
+        toolBar.addSeparator();
+
+        // Open new book
+        JButton openBook = new JButton();
+        openBook.setIcon(new ImageIcon("./Resources/icons8-literature-64.png"));
+        openBook.setPreferredSize(new Dimension(50, 50));
+        openBook.addActionListener(event -> addBibleDialog());
+
+        toolBar.add(openBook);
+
         // Import Button
         JButton importBook = new JButton();
         importBook.setIcon(new ImageIcon("./Resources/icons8-import-50.png"));
         importBook.setPreferredSize(new Dimension(50, 50));
         importBook.setToolTipText("Import a new Bible");
-        importBook.addActionListener(event -> {
-            importDialog();
-        });
+        importBook.addActionListener(event -> importDialog());
 
         toolBar.add(importBook);
         toolBar.setRollover(true);
         toolBar.setFloatable(false);
         this.getContentPane().add(toolBar, BorderLayout.NORTH);
+    }
+
+    private void createReaderTabbedPane() {
+        tabbedPane = new JTabbedPane();
+
+        addReaderPane(bibles.get(0));
+    }
+
+    private void addReaderPane(Bible bible) {
+        int index = tabbedPane.getTabCount();
+        ReaderPanel readerPanel = new ReaderPanel(bible.getBibleId(), 1L, 1L);
+
+        // Creates Tab Label and close button
+        JPanel tabPanel = new JPanel();
+        tabPanel.setOpaque(false);
+        tabPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JLabel label = new JLabel(bible.getBibleShortName());
+        JButton closeButton = new JButton();
+        closeButton.setIcon(new ImageIcon("./Resources/icons8-x-18.png"));
+        closeButton.setPreferredSize(new Dimension(18, 18));
+        closeButton.setToolTipText("Close Tab");
+        closeButton.addActionListener(event -> {
+            tabbedPane.removeTabAt(tabbedPane.indexOfTabComponent(tabPanel));
+            if (tabbedPane.getTabCount() == 0) {
+                emptyTabbedPane();
+            }
+        });
+        tabPanel.add(closeButton);
+        tabPanel.add(label);
+
+        // Adds Panel to the tabbed pane
+        tabbedPane.insertTab(bible.getBibleShortName(), null, readerPanel, bible.getBibleName(), index);
+        tabbedPane.setTabComponentAt(index, tabPanel);
+    }
+
+    private void emptyTabbedPane() {
+        emptyPanel = new JPanel();
+        emptyPanel.setLayout(new BorderLayout());
+
+        // Empty message
+        JLabel label = new JLabel("Nothing to display");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        emptyPanel.add(label, BorderLayout.CENTER);
+        // Sets tabbed pane to not show and displays empty message
+        tabbedPane.setVisible(false);
+        splitPane.remove(tabbedPane);
+        splitPane.add(emptyPanel);
     }
 
     private void importDialog() {
@@ -200,6 +266,81 @@ public class Application extends JFrame {
     }
 
     public void navigateTo(Long book, Long chapter) {
+        ReaderPanel readerPanel = (ReaderPanel) tabbedPane.getSelectedComponent();
         readerPanel.setSearchFields(1L, book, chapter);
+    }
+
+    public void getBibles() {
+        try {
+            DatabaseConnection connection = new DatabaseConnection();
+            bibles = connection.getBibles();
+            connection.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Error connecting to database", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void addBibleDialog() {
+        JDialog dialog = new JDialog(this, "Select a Bible");
+        dialog.setLayout(new BorderLayout());
+        dialog.setResizable(false);
+        dialog.setSize(new Dimension(400, 150));
+        dialog.setLocation((this.getWidth() / 2) + this.getX() - 200, (this.getHeight() / 2) + this.getY() - 75);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        // Make Labels
+        JPanel labelPanel = new JPanel();
+        setLayout(new BorderLayout());
+        JLabel icon = new JLabel(new ImageIcon("./Resources/icons8-literature-64.png"));
+        JLabel label = new JLabel("Please select a Bible");
+        label.setFont(new Font("Serif", Font.PLAIN, 18));
+        labelPanel.add(icon);
+        labelPanel.add(label);
+
+        // Drop Down
+        JPanel dropDownPanel = new JPanel();
+        JComboBox comboBox = new JComboBox<>();
+        comboBox.addItem("-- Select a Bible --");
+        for (Bible bible : bibles) {
+            comboBox.addItem(bible.getBibleName());
+        }
+        dropDownPanel.add(comboBox);
+
+        // Buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BorderLayout());
+        buttonPanel.setBorder(new EmptyBorder(10,10,10,10));
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(e -> {
+            dialog.setVisible(false);
+            dialog.dispose();
+        });
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBackground(Color.BLUE);
+        submitButton.setForeground(Color.WHITE);
+        submitButton.addActionListener(e -> {
+            for (Bible bible : bibles) {
+                if (comboBox.getSelectedItem().toString().equals(bible.getBibleName())) {
+                    addReaderPane(bible);
+                }
+            }
+            if (tabbedPane.getTabCount() == 1) {
+                tabbedPane.setVisible(true);
+                splitPane.remove(emptyPanel);
+                splitPane.add(tabbedPane);
+            }
+            dialog.setVisible(false);
+            dialog.dispose();
+        });
+        buttonPanel.add(cancelButton, BorderLayout.WEST);
+        buttonPanel.add(submitButton, BorderLayout.EAST);
+
+        panel.add(labelPanel, BorderLayout.NORTH);
+        panel.add(dropDownPanel, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+        dialog.add(panel);
+        dialog.setVisible(true);
     }
 }
