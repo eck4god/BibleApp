1ba0c1c975dcdbee0067d99ba8d1b00248f2db58
khaegenm1 <khagenbaro1997@gmail.com>
2023-08-10 23:38:41 +0530

    path in the createbackground added.

--- src/main/java/Views/WelcomeScreen.java
+++ src/main/java/Views/WelcomeScreen.java
@@ -1,9 +1,13 @@
 package main.java.Views;
 
+import main.java.Service.ProgramDirectoryService;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class WelcomeScreen extends JFrame {
+
+    ProgramDirectoryService programDirectoryService = new ProgramDirectoryService();
 
     public WelcomeScreen() {
         this.setUndecorated(true);
@@ -16,7 +20,8 @@ public class WelcomeScreen extends JFrame {
     }
 
     private void createBackground() {
-        ImageIcon img = new ImageIcon("./Resources/bible.jpg");
+        String path =  programDirectoryService.getProgramDirectory();
+        ImageIcon img = new ImageIcon(path+"/Resources/bible.jpg");
 
         JPanel panel = new JPanel() {
             @Override
