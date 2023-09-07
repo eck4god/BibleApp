ef5eee6557819db189afb48a0fe0c04110cd35d7
Gregory Echelberry <gechelberry@aol.clom>
2023-09-07 11:47:02 -0400

    Improved appearance of the Splash Screen and added version

--- src/main/java/Views/WelcomeScreen.java
+++ src/main/java/Views/WelcomeScreen.java
@@ -7,23 +7,24 @@ import java.awt.*;
 
 public class WelcomeScreen extends JFrame {
 
-    ProgramDirectoryService programDirectoryService = new ProgramDirectoryService();
-    public WelcomeScreen() {
+    private final String path;
+    private final String version;
+    public WelcomeScreen(String version) {
+        this.version = version;
         this.setUndecorated(true);
         this.setSize(new Dimension(648, 348));
         this.setResizable(false);
         this.setLocationRelativeTo(null);
 
+        ProgramDirectoryService programDirectoryService = new ProgramDirectoryService();
+        path = programDirectoryService.getProgramDirectory();
 
         createBackground();
     }
 
     private void createBackground() {
 
-
-        String path = programDirectoryService.getProgramDirectory();
-
-        ImageIcon img = new ImageIcon(path+"/Resources/Images/bible.jpg");
+        ImageIcon img = new ImageIcon(path+"/Resources/Images/bibles.jpg");
 
         JPanel panel = new JPanel() {
             @Override
@@ -37,10 +38,10 @@ public class WelcomeScreen extends JFrame {
         JPanel textPanel = new JPanel();
         textPanel.setLayout(new BorderLayout());
         textPanel.setOpaque(false);
-        JLabel title = new JLabel("Bible Reader");
-        title.setHorizontalAlignment(SwingConstants.CENTER);
-        title.setFont(new Font("Serif", Font.BOLD, 32));
-        textPanel.add(title, BorderLayout.CENTER);
+        JLabel version = new JLabel("Version: " + this.version);
+        version.setHorizontalAlignment(SwingConstants.LEFT);
+        version.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+        textPanel.add(version, BorderLayout.SOUTH);
 
         panel.add(textPanel);
         this.getContentPane().add(panel);
